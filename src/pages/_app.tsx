7f0aad40fa9ae0e3cf8c9434cb2d9c4e284b4359
Leonardo Dias Lissone <leonardo.lissonez@gmail.com>
2021-05-25 15:11:24 -0300

    add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
-import '../styles/global.scss'
+import { PlayerContextProvider } from '../contexts/PlayerContext'
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
 
-import { PlayerContextProvider } from '../contexts/PlayerContext';
-import { Header } from '../components/Header';
-import { Player } from '../components/Player';
-
+import { GlobalStyle } from '../styles/global'
 import styles from '../styles/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
     <PlayerContextProvider>
+      <GlobalStyle />
+      
       <div className={styles.wrapper}>
         <main>
           <Header />
